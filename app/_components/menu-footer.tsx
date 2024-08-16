b64a7740171e05ecfc919c45a9a4d3dc3f23c938
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-15 22:23:55 -0500

    feat(cart): :sparkles: Add badge to footer.
    Add badge that displays number of items in cart.

--- app/_components/menu-footer.tsx
+++ app/_components/menu-footer.tsx
@@ -1,9 +1,22 @@
+'use client'
+
+import { useCartContext } from '../_context/cart.context'
+import { BadgeItem } from './cart-badge'
 import { CartMenu } from './cart-menu'
 
 export const MenuFooter = () => {
+  const { cartContent } = useCartContext()
+
   return (
-    <div className='MENU_FOOTER fixed border-t border-neutral-400 bottom-0 bg-neutral-50 w-full p-md'>
+    <div className='MENU_FOOTER flex items-center justify-between gap-md fixed border-t border-neutral-400 bottom-0 bg-neutral-50 w-full p-md'>
       <CartMenu />
+      {cartContent !== null && (
+        <BadgeItem
+          text={`Your Cart - ${cartContent?.length} Item${
+            cartContent?.length > 1 ? 's' : ''
+          }`}
+        />
+      )}
     </div>
   )
 }
