4876872514e8b6eccee62faf121d4206c080ba56
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-18 14:41:37 -0500

    refactor(home): :recycle: Remove footer and product list. Refactor styling of remaining elements.

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,25 @@
-import { ProductList } from "./_components/product-list";
-import { MenuFooter } from "./_components/menu-footer";
+import { ProductCategoryList } from "./_components/product-category-list";
 
 export default async function Home() {
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-start gap-8 p-md pt-xl'>
+    <>
       <h1 className='text-4xl'>
         <span className='font-bold'>Standing O Cosmetics: </span>
-        <span className='font-light'> Stage Ready Order Form</span>
+        <span className='font-light'>
+          {' '}
+          Stage Ready Order Form
+        </span>
       </h1>
-      <p className="text-lg text-left lg:text-center w">Welcome Stage Ready customer! To get started, select from the products below. When you&#39;re ready to submit your order, click the submit button. Your order will be saved, and we will contact you for confirmation after applying your program discount.</p>
-      <ProductList />
-      <MenuFooter />
-    </main>
+      <p className='text-lg text-left lg:text-center w'>
+        Welcome Stage Ready customer! To get started, select from
+        the products below. When you&#39;re ready to submit your
+        order, click the submit button. Your order will be saved,
+        and we will contact you for confirmation after applying
+        your program discount.
+      </p>
+      <ProductCategoryList />
+    </>
   )
 }
+ 
