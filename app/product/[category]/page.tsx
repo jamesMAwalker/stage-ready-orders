d3846caa18596cafbf8f16b59675945b6724bc10
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-18 14:42:37 -0500

    feat(product): :sparkles: Add new product page.

---
+++ app/product/[category]/page.tsx
@@ -0,0 +1,23 @@
+'use client'
+
+import { ProductCategoryList } from '@/app/_components/product-category-list'
+import { ProductList } from '@/app/_components/product-list'
+import { useGetProductCategoryFromLS } from '@/app/_context/hooks/useGetProductCategory'
+import React from 'react'
+
+const ProductCategoryPage = ({
+  params
+}: {
+  params: { category: string }
+}) => {
+  const category = useGetProductCategoryFromLS(params.category)
+
+  return (
+    <div className='full flex-col-tl gap-md'>
+      <h1 className='w-full text-4xl font-bold'>{category?.title} Collection</h1>  
+      <ProductList products={category?.products} />
+    </div>
+  )
+}
+
+export default ProductCategoryPage
