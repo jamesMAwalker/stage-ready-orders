dd865009e09a1d88beddc1653dc2cf4bb03cb587
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-27 21:13:12 -0500

    feat(layout): :sparkles: Add loading page.

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingPage = () => {
+  return (
+    <div className='flex-col-c w-full h-[80vh]'>
+      <img src='/images/soc-logo.png' alt='logo' className='w-[10vw] h-auto animate-pulse' />
+    </div>
+  )
+}
+
+export default LoadingPage
