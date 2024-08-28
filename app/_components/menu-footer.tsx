523a3cd9ad967f9233ff711635641d55aa366553
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-27 21:43:44 -0500

    refactor(cart): :sparkles: Add cart total display.

--- app/_components/menu-footer.tsx
+++ app/_components/menu-footer.tsx
@@ -9,16 +9,20 @@ import { CartMenu } from './cart-menu'
 export const MenuFooter = () => {
   const { cartContent } = useCartContext()
 
+  const totalQauntity = cartContent?.reduce((acc, item) => {
+    return acc + item.quantity
+  }, 0)
+
   return (
     <div className='MENU_FOOTER flex items-center justify-between gap-md fixed border-t border-neutral-400 bottom-0 bg-neutral-50 w-full p-ms lg:p-md'>
       <CartMenu />
-      {cartContent !== null && (
-        <BadgeItem classes='flex-c gap-sm'>
+      {cartContent && cartContent?.length > 0 && (
+        <BadgeItem classes='flex-c gap-sm border-green-500 rounded-md'>
           <ShoppingCartIcon />
           <span>▪</span>
           <span>
-            {cartContent?.length} Item
-            {cartContent?.length > 1 ? 's' : ''}
+            {totalQauntity} Item
+            {Number(totalQauntity) > 1 ? 's' : ''}
           </span>
         </BadgeItem>
       )}
